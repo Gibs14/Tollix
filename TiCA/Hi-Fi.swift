9ca228434a79e20b8a0bd17ebd90f7a997748051
Gibran Shevaldo <zen@Gibrans-MacBook-Pro.local>
2025-07-21 17:16:35 +0700

    Release: Baseline Dev POC Tollix

---
+++ TiCA/Hi-Fi.swift
@@ -0,0 +1,18 @@
+//
+//  Hi-Fi.swift
+//  TiCA
+//
+//  Created by Louis Oktovianus on 21/07/25.
+//
+
+import SwiftUI
+
+struct Hi_Fi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Hi_Fi()
+}
